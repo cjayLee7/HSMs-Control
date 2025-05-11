ce91e97d30595b125ab7cbfd9c950506bb336cea
cjayLee7 <95034698+cjayLee7@users.noreply.github.com>
2025-05-11 19:07:08 +0800

    singal_input.m

--- MATLAB/singal_input.m
+++ MATLAB/singal_input.m
@@ -1,19 +1,19 @@
 function acc=singal_input3(omega_d,t)
-t_a=0.2; %加速时间0.2s
+t_a=0.2; %Acceleration time 0.2s
 a=omega_d*2/t_a;
 if t<0.1
     acc=0;
 end
 
-if t>=0.1 && t<(0.1+t_a) %加速段
+if t>=0.1 && t<(0.1+t_a) %Acceleration
     acc=a/t_a*(0.1+t_a)-a/t_a*t;
 end
 
-if t>=(0.1+t_a) && t<=(0.9-t_a) %匀速段
+if t>=(0.1+t_a) && t<=(0.9-t_a) %Uniform speed
     acc=0;
 end
 
-if t>=(0.9-t_a) && t<=0.9 %减速段
+if t>=(0.9-t_a) && t<=0.9 %Slow down
     acc=a/t_a*(0.9-t_a)-a/t_a*t;
 end
 
